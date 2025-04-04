47665428166edba63ce373dddf370d1710fe7009
cbe100 <engr.cbe100@gmail.com>
2025-04-04 19:07:58 +0800

    Update ZMPT101B.h

--- esphome/components/zmpt101b/ZMPT101B.h
+++ esphome/components/zmpt101b/ZMPT101B.h
@@ -1,35 +1,36 @@
-#ifndef ZMPT101B_h
-#define ZMPT101B_h
+#ifndef ZMPT101B_H
+#define ZMPT101B_H
 
-#include "esphome.h"
-#include <Arduino.h>
+#include "esphome.h"  // Include ESPHome's base header
 
 #define DEFAULT_FREQUENCY 50.0f
 #define DEFAULT_SENSITIVITY 500.0f
 
-#if defined(AVR)
-	#define ADC_SCALE 1023.0f
-	#define VREF 5.0f
-#elif defined(ESP8266)
-	#define ADC_SCALE 1023.0
-	#define VREF 3.3
-#elif defined(ESP32)
-	#define ADC_SCALE 4095.0
-	#define VREF 3.3
-#endif
-
-class ZMPT101B
-{
+class ZMPT101B : public PollingComponent, public Sensor {
 public:
-	ZMPT101B (uint8_t pin, uint16_t frequency = DEFAULT_FREQUENCY);
-	void     setSensitivity(float value);
-	float 	 getRmsVoltage(uint8_t loopCount = 1);
+    ZMPT101B(uint8_t pin, uint16_t frequency = DEFAULT_FREQUENCY)
+        : PollingComponent(1000), pin_(pin), frequency_(frequency) {}  // Poll every second (1000ms)
+
+    void setSensitivity(float value) { sensitivity_ = value; }
+
+    float getRmsVoltage(uint8_t loopCount = 1);
+
+    void setup() override {
+        pinMode(pin_, INPUT);
+    }
+
+    void update() override {
+        // Call the method to read the RMS voltage and update the state
+        float voltage = getRmsVoltage(); // Call your method for RMS voltage
+        publish_state(voltage);          // Publish the result to ESPHome
+    }
 
 private:
-	uint8_t  pin;
-	uint32_t period;
-	float 	 sensitivity = DEFAULT_SENSITIVITY;
-	int 	 getZeroPoint();
+    uint8_t pin_;
+    uint16_t frequency_;
+    float sensitivity_ = DEFAULT_SENSITIVITY;
+
+    int getZeroPoint();
 };
 
 #endif
