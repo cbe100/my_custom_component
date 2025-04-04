86a2b2615fd262647f5283227ea07dbaa6fbb47a
cbe100 <engr.cbe100@gmail.com>
2025-04-04 19:07:19 +0800

    Update ZMPT101B.cpp

--- esphome/components/zmpt101b/ZMPT101B.cpp
+++ esphome/components/zmpt101b/ZMPT101B.cpp
@@ -1,64 +1,52 @@
 #include "ZMPT101B.h"
+#include <cmath>
 
 /// @brief ZMPT101B constructor
-/// @param pin analog pin that ZMPT101B connected to.
+/// @param pin analog pin that ZMPT101B is connected to.
 /// @param frequency AC system frequency
 ZMPT101B::ZMPT101B(uint8_t pin, uint16_t frequency)
+    : PollingComponent(1000) // Poll every second (1000ms)
 {
-	this->pin = pin;
-	period = 1000000 / frequency;
-	pinMode(pin, INPUT);
+    this->pin_ = pin;
+    period = 1000000 / frequency;  // Microseconds per cycle
 }
 
-/// @brief Set sensitivity
-/// @param value Sensitivity value
-void ZMPT101B::setSensitivity(float value)
-{
-	sensitivity = value;
+void ZMPT101B::setSensitivity(float value) {
+    sensitivity_ = value;
 }
 
-/// @brief Calculate zero point
-/// @return zero / center value
-int ZMPT101B::getZeroPoint()
-{
-	uint32_t Vsum = 0;
-	uint32_t measurements_count = 0;
-	uint32_t t_start = micros();
+int ZMPT101B::getZeroPoint() {
+    uint32_t Vsum = 0;
+    uint32_t measurements_count = 0;
+    uint32_t t_start = micros();
 
-	while (micros() - t_start < period)
-	{
-		Vsum += analogRead(pin);
-		measurements_count++;
-	}
+    while (micros() - t_start < period) {
+        Vsum += analogRead(pin_);
+        measurements_count++;
+    }
 
-	return Vsum / measurements_count;
+    return Vsum / measurements_count;
 }
 
-/// @brief Calculate root mean square (RMS) of AC valtage
-/// @param loopCount Loop count to calculate
-/// @return root mean square (RMS) of AC valtage
-float ZMPT101B::getRmsVoltage(uint8_t loopCount)
-{
-	double readingVoltage = 0.0f;
+float ZMPT101B::getRmsVoltage(uint8_t loopCount) {
+    double readingVoltage = 0.0f;
 
-	for (uint8_t i = 0; i < loopCount; i++)
-	{
-		int zeroPoint = this->getZeroPoint();
+    for (uint8_t i = 0; i < loopCount; i++) {
+        int zeroPoint = this->getZeroPoint();
 
-		int32_t Vnow = 0;
-		uint32_t Vsum = 0;
-		uint32_t measurements_count = 0;
-		uint32_t t_start = micros();
+        int32_t Vnow = 0;
+        uint32_t Vsum = 0;
+        uint32_t measurements_count = 0;
+        uint32_t t_start = micros();
 
-		while (micros() - t_start < period)
-		{
-			Vnow = analogRead(pin) - zeroPoint;
-			Vsum += (Vnow * Vnow);
-			measurements_count++;
-		}
+        while (micros() - t_start < period) {
+            Vnow = analogRead(pin_) - zeroPoint;
+            Vsum += (Vnow * Vnow);
+            measurements_count++;
+        }
 
-		readingVoltage += sqrt(Vsum / measurements_count) / ADC_SCALE * VREF * sensitivity;
-	}
+        readingVoltage += sqrt(Vsum / measurements_count) / ADC_SCALE * VREF * sensitivity_;
+    }
 
-	return readingVoltage / loopCount;
+    return readingVoltage / loopCount;
 }
